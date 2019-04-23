5bbb1f5b871cf1160148e681e56485f234efd635
PiotrTopolski <piotr.topolski88@gmail.com>
2019-04-23 22:06:56 +0200

    end of work for today, develop adding markers per click

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 14);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 16
 }).addTo(mymap);
+
+function onMapClick(e) {
+  alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
