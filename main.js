a2f129d88d6dc0a57b47df69ee5bb4b23aec90ee
PiotrTopolski <piotr.topolski88@gmail.com>
2019-04-24 15:23:56 +0200

    all functions added

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
+const tbody = document.querySelector('.table tbody');
 const mymap = L.map('mapid').setView([53.435022, 14.569659], 14);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   maxZoom: 16,
 }).addTo(mymap);
 const markerList = [];
+let i = 0;
+
+const addTableRow = (markerList) => {
+  const tr = document.createElement('tr');
+  tbody.appendChild(tr);
+  const thIndex = document.createElement('th');
+  const tdLat = document.createElement('td');
+  const tdLng = document.createElement('td');
+  tr.appendChild(thIndex);
+  tr.appendChild(tdLat).textContent = `${markerList[i].getLatLng().lat}`;
+  tr.appendChild(tdLng).textContent = `${markerList[i].getLatLng().lng}`;
+  thIndex.textContent = ++i;
+}
 
 const onMapClick = e => {
   const marker = new L.marker(e.latlng, {
-    draggable: 'true'
+    draggable: 'true',
   });
   markerList.push(marker);
   marker.on('dragend', e => {
@@ -17,8 +31,14 @@ const onMapClick = e => {
       draggable: 'true'
     });
     mymap.panTo(new L.LatLng(position.lat, position.lng))
+
+    let index = markerList.indexOf(e.target);
+    const tdLatDragend = document.querySelector(`.table tr:nth-of-type(${index + 1}) td:first-of-type`);
+    const tdLngDragend = document.querySelector(`.table tr:nth-of-type(${index + 1}) td:last-of-type`)
+    tdLatDragend.textContent = `${position.lat}`;
+    tdLngDragend.textContent = `${position.lng}`;
   });
   mymap.addLayer(marker);
+  addTableRow(markerList);
 };
-
 mymap.on('click', onMapClick);
