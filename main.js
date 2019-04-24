8090ff85ae035a824d1d8711bc203733b4b53224
PiotrTopolski <piotr.topolski88@gmail.com>
2019-04-24 13:14:08 +0200

    marker add and dragen

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 14);
+const mymap = L.map('mapid').setView([53.435022, 14.569659], 14);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 16
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  maxZoom: 16,
 }).addTo(mymap);
+const markerList = [];
 
-function onMapClick(e) {
-  alert("You clicked the map at " + e.latlng);
-}
+const onMapClick = e => {
+  const marker = new L.marker(e.latlng, {
+    draggable: 'true'
+  });
+  markerList.push(marker);
+  marker.on('dragend', e => {
+    const marker = e.target;
+    const position = marker.getLatLng();
+    marker.setLatLng(new L.LatLng(position.lat, position.lng), {
+      draggable: 'true'
+    });
+    mymap.panTo(new L.LatLng(position.lat, position.lng))
+  });
+  mymap.addLayer(marker);
+};
 
 mymap.on('click', onMapClick);
